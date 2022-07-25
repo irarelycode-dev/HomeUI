ef999d5c393b55daa45869df570d46e00baa40c4
vignesh pugazhendhi <irarelycode@gmail.com>
2022-07-25 23:21:56 +0530

    home ui navigators added

---
+++ navigators/StackNavigator.jsx
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../views/screens/Home";
+import Onboard from "../views/screens/Onboard";
+import Details from "../views/screens/Details";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen name="onboard" component={Onboard} />
+      <Stack.Screen name="details" component={Details} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
